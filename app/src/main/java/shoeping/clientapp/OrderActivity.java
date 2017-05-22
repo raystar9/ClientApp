bf0037c27d75a9cb76ca23da439d2d7361be2f53
Koo <uther9@naver.com>
2017-05-22 11:21:21 +0900

    OrderActivity에 뷰 링크

--- app/src/main/java/shoeping/clientapp/OrderActivity.java
+++ app/src/main/java/shoeping/clientapp/OrderActivity.java
@@ -2,7 +2,9 @@ package shoeping.clientapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class OrderActivity extends AppCompatActivity {
 
@@ -11,6 +13,12 @@ public class OrderActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
 
-        EditText
+        TextView textView_id = (TextView) findViewById(R.id.idTxv);
+        EditText editText_adress = (EditText) findViewById(R.id.adressEdt);
+        EditText editText_phone = (EditText) findViewById(R.id.phoneEdt);
+        EditText editText_comment = (EditText) findViewById(R.id.commentEdt);
+        Button button_order = (Button) findViewById(R.id.orderBtn);
+
+        
     }
 }
